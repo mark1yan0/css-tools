25851507cbff5a46fa1683e057ca9aae8aaccf32
mark1yan0 <markiyan.dev@gmail.com>
2024-08-22 18:07:12 +0200

    initial development and min working product - converts rem to px and back

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	// import Input from './lib/Input.svelte';
+	let base = 16;
+	let rem;
+
+	$: px = !rem ? null : rem * base;
+</script>
+
+<main>
+	<!-- <Input name="rem" label="REM" />
+	<Input name="px" label="PX" /> -->
+
+	<label for="base">
+		Base
+		<input id="base" type="number" name="base" bind:value={base} />
+	</label>
+
+	<label for="rem">
+		REM
+		<input id="rem" type="number" name="rem" bind:value={rem} />
+	</label>
+
+	<label for="px">
+		PX
+		<input
+			id="px"
+			type="number"
+			name="px"
+			bind:value={px}
+			on:input={() => {
+				rem = px / base;
+			}}
+		/>
+	</label>
+</main>
---
+++ src/lib/Input.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let name: string,
+		label: string,
+		onInput: svelte.JSX.FormEventHandler<HTMLInputElement>;
+</script>
+
+<label for={name}>
+	{label}
+	<input id={name} type="number" {name} on:input={onInput} />
+</label>
