6497ccc04685b36298d357f7e4133aee527ece6b
mark1yan0 <markiyan.dev@gmail.com>
2025-02-15 14:59:12 +0100

    migrates project to svelte 5

--- src/App.svelte
+++ src/App.svelte
@@ -1,53 +1,53 @@
 <script lang="ts">
-	import Input from './lib/Input.svelte';
-	let base = 16;
-	let rem;
-	$: px = !rem ? null : rem * base;
+    import Input from "./lib/Input.svelte";
+    let base = 16;
+    let rem = 1;
+    $: px = !rem ? base : rem * base;
 </script>
 
 <main>
-	<section class="converter">
-		<Input name="rem" label="REM" unit="rem" copyOnClick bind:value={rem} />
-		<Input
-			name="px"
-			label="PX"
-			unit="px"
-			copyOnClick
-			bind:value={px}
-			onInput={() => {
-				rem = px / base;
-			}}
-		/>
-	</section>
+    <section class="converter">
+        <Input name="rem" label="REM" unit="rem" copyOnClick bind:value={rem} />
+        <Input
+            name="px"
+            label="PX"
+            unit="px"
+            copyOnClick
+            bind:value={px}
+            onInput={() => {
+                rem = px / base;
+            }}
+        />
+    </section>
 
-	<footer class="configs">
-		<Input name="base" label="Base" bind:value={base} />
-	</footer>
+    <footer class="configs">
+        <Input name="base" label="Base" bind:value={base} />
+    </footer>
 </main>
 
 <style>
-	main {
-		display: flex;
-		flex-direction: column;
-		gap: 1rem;
-	}
+    main {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+    }
 
-	section.converter {
-		display: flex;
-		flex-direction: column;
-		gap: 1rem;
-	}
+    section.converter {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+    }
 
-	@media only screen and (min-width: 1100px) {
-		section.converter {
-			flex-direction: row;
-		}
-	}
+    @media only screen and (min-width: 1100px) {
+        section.converter {
+            flex-direction: row;
+        }
+    }
 
-	footer {
-		padding: 0.5rem;
-		position: fixed;
-		bottom: 0;
-		left: 0;
-	}
+    footer {
+        padding: 0.5rem;
+        position: fixed;
+        bottom: 0;
+        left: 0;
+    }
 </style>
--- src/lib/Input.svelte
+++ src/lib/Input.svelte
@@ -1,67 +1,68 @@
 <script lang="ts">
-	import CopySvg from './CopySVG.svelte';
+    import type { FormEventHandler } from "svelte/elements";
+    import CopySvg from "./CopySVG.svelte";
 
-	export let name: string,
-		label: string,
-		value: string | number,
-		unit: string = undefined,
-		copyOnClick: boolean = undefined,
-		onInput: svelte.JSX.FormEventHandler<HTMLInputElement> = undefined;
+    export let name: string,
+        label: string,
+        value: string | number,
+        unit: string | null = null,
+        copyOnClick: boolean | null = null,
+        onInput: FormEventHandler<HTMLInputElement> | null = null;
 
-	async function copyOnClickHandler() {
-		if (!unit) {
-			throw new Error('A unit must be provided');
-		}
+    async function copyOnClickHandler() {
+        if (!unit) {
+            throw new Error("A unit must be provided");
+        }
 
-		if ('clipboard' in navigator) {
-			await navigator.clipboard.writeText(
-				`${value.toString().replace(',', '.')}${unit}`
-			);
-		}
-	}
+        if ("clipboard" in navigator) {
+            await navigator.clipboard.writeText(
+                `${value.toString().replace(",", ".")}${unit}`,
+            );
+        }
+    }
 </script>
 
 <label for={name}>
-	{label}
+    {label}
 
-	<span class="input-wrapper">
-		{#if copyOnClick}
-			<button on:click={copyOnClickHandler}>
-				<CopySvg />
-			</button>
-		{/if}
-		<input id={name} type="number" {name} bind:value on:input={onInput} />
-	</span>
+    <span class="input-wrapper">
+        {#if copyOnClick}
+            <button on:click={copyOnClickHandler}>
+                <CopySvg />
+            </button>
+        {/if}
+        <input id={name} type="number" {name} bind:value on:input={onInput} />
+    </span>
 
-	{#if unit}
-		<span class="unit-span">{unit}</span>
-	{/if}
+    {#if unit}
+        <span class="unit-span">{unit}</span>
+    {/if}
 </label>
 
 <style>
-	label {
-		display: flex;
-		flex-direction: column;
-		align-items: flex-start;
-		gap: 0.5rem;
-		position: relative;
-	}
+    label {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        gap: 0.5rem;
+        position: relative;
+    }
 
-	.input-wrapper {
-		display: flex;
-		align-items: center;
-		gap: 1rem;
-	}
+    .input-wrapper {
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+    }
 
-	button {
-		border-radius: 100em;
-		/* padding: 1rem; */
-	}
+    button {
+        border-radius: 100em;
+        /* padding: 1rem; */
+    }
 
-	label > .unit-span {
-		position: absolute;
-		top: 53%;
-		right: 5%;
-		background-color: var(--dark-darker);
-	}
+    label > .unit-span {
+        position: absolute;
+        top: 53%;
+        right: 5%;
+        background-color: var(--dark-darker);
+    }
 </style>
