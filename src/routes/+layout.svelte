10570ae668754dd699384267a12a33a051c384ff
mark1yan0 <markiyan.dev@gmail.com>
2025-03-13 01:50:42 +0100

    #1 - layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import Header from "@/ui/components/Header.svelte";
-  import "../app.css";
+  import Header from "@/ui/layouts/Header.svelte";
+  import Footer from "@/ui/layouts/Footer.svelte";
 
   let { children } = $props();
 </script>
@@ -12,16 +12,12 @@
     {@render children()}
   </main>
 
-  <footer>
-    <p>
-      visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to learn
-      about SvelteKit
-    </p>
-  </footer>
+  <Footer />
 </div>
 
 <style>
   .app {
+    /* @apply flex flex-col min-h-screen bg-red-400; */
     display: flex;
     flex-direction: column;
     min-height: 100vh;
@@ -36,23 +32,5 @@
     max-width: 64rem;
     margin: 0 auto;
     box-sizing: border-box;
-  }
-
-  footer {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 12px;
-  }
-
-  footer a {
-    font-weight: bold;
-  }
-
-  @media (min-width: 480px) {
-    footer {
-      padding: 12px 0;
-    }
   }
 </style>
---
+++ src/ui/layouts/Footer.svelte
@@ -0,0 +1,17 @@
+<footer>footer</footer>
+
+<style>
+  footer {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 12px;
+  }
+
+  @media (min-width: 480px) {
+    footer {
+      padding: 12px 0;
+    }
+  }
+</style>
