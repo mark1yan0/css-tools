1b6d1d7e333a8ef63d6880afbaa963927ba09b42
mark1yan0 <markiyan.dev@gmail.com>
2025-03-14 21:09:00 +0100

    changes folder structure + refactors into components + uses correct icon

--- src/components/fields/Input.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	export const name = null;
-</script>
-
-<input type="number" {name} />
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import tailwindcss from "@tailwindcss/vite";
+import * as path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [svelte(), tailwindcss()],
-	server: {
-		open: true,
-	},
+  plugins: [svelte(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
