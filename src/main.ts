6010d92ea494de59ce5fc5c57ec2ddb54c1c5b4a
mark1yan0 <markiyan.dev@gmail.com>
2025-03-28 20:13:16 +0100

    adds dev styles for debugging

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
-import { mount } from 'svelte'
-import './app.css'
-import App from './App.svelte'
+import { mount } from "svelte";
+import "./app.css";
+import App from "./App.svelte";
+
+if (import.meta.env.DEV) {
+    import("./dev.css").then(() => {
+        document.getElementById("app")!.classList.add("development");
+    });
+}
 
 const app = mount(App, {
-  target: document.getElementById('app')!,
-})
+    target: document.getElementById("app")!,
+});
 
-export default app
+export default app;
