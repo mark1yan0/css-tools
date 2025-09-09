96441b1271c3db05ee11375fe8dba0bd3b13c397
mark1yan0 <markiyan.dev@gmail.com>
2025-09-10 01:17:41 +0200

    removed folders

--- src/ui/layouts/header/index.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import Logo from "@/ui/components/Logo/index.svelte";
-    import Nav from "./Nav.svelte";
-</script>
-
-<header id="header" class="p-4 flex items-center justify-between">
-    <Logo />
-    <Nav />
-</header>
